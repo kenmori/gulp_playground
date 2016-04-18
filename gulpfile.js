f4c6cd55e72fbfbed3ecadb8b2dbe09c0ba12b71
kenji <kennjimoritata@yahoo.co.jp>
2016-04-18 21:23:33 +0900

    write scss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 
-gulp('default', ['watch'], function(){
-  gulp.src('')
-  .pipe()
-  .pipe();
+
+var config = {
+    scssTarget: ['./src/assets/**/*.scss', '!./src/assets/published/']
+};
+
+gulp.task('watch', function(){
+  gulp.watch('src/assets/styles/**.scss', ['scss'] );
 });
+
+gulp.task('scss', function(){
+    gulp.src(config.scssTarget)
+    .pipe(sass())
+    .pipe(gulp.dist('dist/assets/'));
+});
+
+gulp.task('default', ['scss']);
